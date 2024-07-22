977261671cd4340aa56f6ec7f04ca63d635feb55
Jess <nomenjanaharimananajessica@gmail.com>
2024-07-22 11:18:52 +0300

    add loading

--- src/app/report/report-object/report-object/report-object.component.ts
+++ src/app/report/report-object/report-object/report-object.component.ts
@@ -10,21 +10,25 @@ import { ReportService } from 'src/app/services/report.service';
 export class ReportObjectComponent implements OnInit {
   @Input() objectId!: number;
   @Input() typeReports: any[] = [];
-
+  
   selectedReason: string = '';
   customReason: string = '';
+  loading: boolean = false;
 
   constructor(public activeModal: NgbActiveModal, private reportService: ReportService) {}
 
   ngOnInit(): void {}
 
   submitReport(): void {
+    this.loading = true;
     const reason = this.customReason || this.selectedReason;
     this.reportService.reportObject(this.objectId, reason).subscribe(
       () => {
+        this.loading = false;
         this.activeModal.close('report submitted');
       },
       (error) => {
+        this.loading = false;
         console.error('Erreur lors de l\'envoi du signalement:', error);
       }
     );
