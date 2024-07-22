3a3cde786c44d5bebcc552a3d9675f035544439c
10Soa <soaornella5@gmail.com>
2024-07-22 18:56:09 +0300

    Update exchanges.module.ts

--- src/app/exchange/exchanges.module.ts
+++ src/app/exchange/exchanges.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
-
+import { MatCardModule } from '@angular/material/card';
 import { SharedModule } from '../shared/shared.module';
 import { ExchangeRoutingModule } from './exchanges-routing.module';
 import { ExchangeInProgressComponent } from './exchange-in-progress/exchange-in-progress.component';
@@ -12,14 +12,20 @@ import { UserObjectsComponent } from './exchange-add/user-objects/user-objects.c
 import { ObjectCardExpandedComponent } from '../object/object-card-expanded/object-card-expanded.component';
 import { ObjectCardComponent } from '../object/object-card/object-card.component';
 import { ExchangeDetailComponent } from './exchange-detail/exchange-detail.component';
-
+import { AcceptExchangeComponent } from './exchange-detail/accept-exchange/accept-exchange.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     ExchangeInProgressComponent,
     ExchangeAddComponent,
     UserObjectsComponent,
-    ExchangeDetailComponent
+    ExchangeDetailComponent,
+    AcceptExchangeComponent
   ],
   imports: [
     CommonModule,
@@ -30,7 +36,13 @@ import { ExchangeDetailComponent } from './exchange-detail/exchange-detail.compo
     MatTableModule,
     MatDialogModule,
     ObjectCardExpandedComponent,
-    ObjectCardComponent
+    ObjectCardComponent,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
 })
 export class ExchangesModule { }
