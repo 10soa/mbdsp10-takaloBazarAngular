b78e2a5ba9394a1ad86bdfb454b473a8c119d885
Jess <nomenjanaharimananajessica@gmail.com>
2024-07-23 21:33:57 +0300

    change API_URL

--- src/app/constants/app.constants.ts
+++ src/app/constants/app.constants.ts
@@ -1,5 +1,5 @@
-// export const API_URL = 'https://takalobazarserver.onrender.com/api';
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'https://takalobazarserver.onrender.com/api';
+// export const API_URL = 'http://localhost:3000/api';
 export const TOKEN_NAME = 'session';
 export const USERID = 'userID';
 export const USERNAME = 'username';
